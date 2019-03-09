0a338e0f3648c031de3fb0871f1e48011ae145c4
NGovani <naim@govani.org>
2019-03-09 21:58:22 +0000

    fixed cyclic dependency

--- full_compiler/include/ast/context.hpp
+++ full_compiler/include/ast/context.hpp
@@ -10,7 +10,10 @@
 #include <unordered_map>
 #include <algorithm>
 
-#include "ast_base_nodes.hpp"
+class Node;
+typedef Node* NodePtr; //never use any member functions for this class
+
+
 
 struct varData{
     int val;
@@ -19,7 +22,7 @@ struct varData{
     int size; //used mainly for arrays but we can initlialise it for normal integers, to be used with the SizeOf command
 
     bool isTypdef = false;
-    //NodePtr typedefLoc = NULL; //points to the declaration specifiers the typedef contains
+    NodePtr typedefLoc = NULL; //points to the declaration specifiers the typedef contains
 
     int offset; //used to specifiy how far this variable is from the stack pointer(frame pointer)? idk
     //varData(int _val, int _size, int _offset,)
